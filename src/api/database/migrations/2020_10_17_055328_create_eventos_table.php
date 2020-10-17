223d9c0dc4ef14e051fc3679afc54c643cb5fd28
Juan Rodas <juan.rodas@pingponginnovation.com>
2020-10-17 01:36:23 -0500

    add Evento

---
+++ src/api/database/migrations/2020_10_17_055328_create_eventos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eventos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp("fechaini")->useCurrent();
+            $table->string('machine');
+            $table->string('namevar');
+            $table->integer('valuevar');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('eventos');
+    }
+}
