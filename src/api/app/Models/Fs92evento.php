c07b5085b8aa903f53e8cead39bf893d3a77c0fb
Juan Rodas <rocas110@hotmail.com>
2020-10-19 01:18:57 +0200

    Fs 92 eventos add

---
+++ src/api/app/Models/Fs92evento.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class fs92evento extends Model
+{
+    use HasFactory;
+    protected $fillable = ['fechaini','machine','namevar','valuevar'];
+
+}
--- src/api/routes/api.php
+++ src/api/routes/api.php
@@ -20,4 +20,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::resource('employee', 'EmployeeController');
 Route::resource('evento', 'EventoController');
+Route::resource('fs92eventos', 'FS_92_EVENTOSController');
 //Route::resource('pokemons', 'PokemonController');
